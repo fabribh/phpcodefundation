afcd21c82212cd98af20c4a87ea3b3db0722bedf
Fabio Ribeiro <si.fabioribeiro@gmail.com>
2015-02-19 17:16:31 -0200

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8" />
+
+    <title></title>
+    <script type="jscript" src="js/bootstrap.min.js"></script>
+    <script type="text/css" src="css/bootstrap-theme.min.css"></script>
+    <script type="text/css" src="css/bootstrap.min.css"></script>
+
+</head>
+
+<body>
+<div class="container">
+    <ul>
+        <li>Home</li>
+        <li>Empresa</li>
+        <li>Produtos</li>
+        <li>Serviços</li>
+        <li>Contato</li>
+    </ul>
+</div>
+
+</body>
+</html>
